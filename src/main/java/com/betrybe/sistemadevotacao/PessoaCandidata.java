476dfc5db4d861e5b63a14f6e0af09d08352b081
iago-barcelos <iago.cmpb@gmail.com>
2024-04-17 11:03:01 -0300

    req 2

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,32 @@ package com.betrybe.sistemadevotacao;
 /**
  * Classe da pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  int numero;
+  int votos;
 
+  /**
+   * Construtor.
+   */
+  public PessoaCandidata(String nome, int numero) {
+    this.numero = numero;
+    this.nome = nome;
+    this.votos = 0;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 }
