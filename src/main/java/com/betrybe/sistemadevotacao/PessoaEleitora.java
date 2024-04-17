6716c7ffdf4ba2bcfcd9039440769fadc3070a82
iago-barcelos <iago.cmpb@gmail.com>
2024-04-17 11:12:25 -0300

    req 3

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -3,6 +3,19 @@ package com.betrybe.sistemadevotacao;
 /**
  * Classe da pessoa Eleitora.
  */
-public class PessoaEleitora{
+public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
 }
